571c62d8545c318c9f4e7f24c37c50298441a7fe
Frescookok <217960655+Frescookok@users.noreply.github.com>
2026-04-03 23:52:58 +0800

    update homepage with event details, speakers info and performers info

--- tedx-cityu/src/Components/Speaker.jsx
+++ tedx-cityu/src/Components/Speaker.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { styled } from "styled-components";
 
 const Container = styled.div``;
@@ -7,37 +7,44 @@ const DescriptionWrapper = styled.div``;
 const TitleText = styled.div``;
 const Picture = styled.img``;
 
-const Speaker = ({data}) =>{
-    useEffect(() => {
-      return () => {
-        console.log(data)
-      }
-    }, [data])
-    
-    return(
-        <Container>
-            {data.map((item, index) => (
-                <Wrapper key={index} className="flex flex-col items-center md:flex-row md:items-start md:gap-5 md:mt-10 md:mb-10 font-textfont text-center md:text-left justify-center">
-                    <Picture
-                        src={require("../Assets/Members/Speaker/" + item.img)}
-                        alt={item.fname}
-                        className="object-scale-down w-48 h-48 md:ml-36 mb-5 md:mb-auto mt-10 md:mt-auto md:my-auto rounded-md"
-                    />
-                    <Wrapper className="flex flex-col items-center md:items-start">
-                        <TitleText className="font-bold text-3xl md:my-0">
-                            {item.fname}
-                        </TitleText>
-                        <DescriptionWrapper className="mx-5 md:mx-0 text-justify text-lg mt-3 md:mt-4 md:mr-36">
-                            {item.desc}
-                        </DescriptionWrapper>
-                        {/* <DescriptionWrapper className="mx-5 md:mx-0 text-lg my-5 md:mt-4 md:mr-36">
-                            {item.link && <SpeakerLink target="_blank" href={item.link}>{item.link}</SpeakerLink>}
-                        </DescriptionWrapper> */}
-                    </Wrapper>
-                </Wrapper>
-            ))}
-        </Container>        
-    );
-}
+const Speaker = ({ data }) => {
+  useEffect(() => {
+    return () => {
+      console.log(data);
+    };
+  }, [data]);
 
-export default Speaker
+  return (
+    <Container>
+      {data.map((item, index) => (
+        <Wrapper
+          key={index}
+          className="flex flex-col md:flex-row items-stretch gap-8 md:gap-12 font-textfont text-center md:text-left justify-center max-w-6xl mx-auto px-4 py-8"
+        >
+          {/* Image column – matches text column height */}
+          <div className="md:w-1/2 flex items-stretch">
+            <Picture
+              src={require("../Assets/Members/Speaker/" + item.img)}
+              alt={item.fname}
+              className="w-full h-auto object-cover rounded-md"
+              style={{ maxHeight: "100%" }}
+            />
+          </div>
+          {/* Text column */}
+          <div className="md:w-1/2 flex flex-col justify-center">
+            <TitleText className="font-bold text-3xl md:text-5xl mb-4 md:mb-6">
+              {item.fname}
+            </TitleText>
+            <DescriptionWrapper className="text-justify text-base md:text-lg leading-relaxed">
+              {item.desc.split('\n').map((para, i) => (
+                <p key={i} className="mb-4">{para}</p>
+              ))}
+            </DescriptionWrapper>
+          </div>
+        </Wrapper>
+      ))}
+    </Container>
+  );
+};
+
+export default Speaker;
